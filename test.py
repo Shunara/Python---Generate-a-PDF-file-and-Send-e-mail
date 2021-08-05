768e5a2889ddd2f04b8cfc75fc2385933726e617
Leonardo <leonardodealbuquerque2@gmail.com>
2021-08-05 20:50:26 -0300

    Added more features to the code

--- test.py
+++ test.py
@@ -5,3 +5,20 @@ import mimetypes
 import smtplib
 import getpass
 
+# Creating an empty message
+message = EmailMessage()
+
+# Substitute the sender and recipient values
+# with any e-mail(s) of your choice
+sender = "me@example.com"
+recipient = "you@example.com"
+message['From'] = sender
+message['To'] = recipient
+
+# Adding a subject to the message
+message['Subject'] = 'Using Python to send an e-mail from {} to {}'.format(sender, recipient)
+
+# Editing the body of the e-mail
+body = """Hey there! I sent this e-mail to you using Python!"""
+message.set_content(body)
+print(message)
